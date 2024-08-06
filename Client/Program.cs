f80a57127e759af6311a3b58f23f5d6aab487f3f
MaxHotiuk <hotiukmax@gmail.com>
2024-08-06 20:21:26 +0300

    started again.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,27 @@
-using QuickChat.Hubs;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSignalR().AddAzureSignalR();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseRouting();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.MapHub<ChatSampleHub>("/chat");
-await app.RunAsync();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
